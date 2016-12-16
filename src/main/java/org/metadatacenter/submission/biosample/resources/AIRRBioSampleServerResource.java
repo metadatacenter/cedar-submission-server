9af49700bac6c78b72bb7a29dbc5dc6f7837f64d
martinjoconnor <martin.oconnor@stanford.edu>
2016-12-16 10:43:23 -0800

    Initial AIRR template instance validation

--- src/main/java/org/metadatacenter/submission/biosample/resources/AIRRBioSampleServerResource.java
+++ src/main/java/org/metadatacenter/submission/biosample/resources/AIRRBioSampleServerResource.java
@@ -1,6 +1,7 @@
 package org.metadatacenter.submission.biosample.resources;
 
 import com.codahale.metrics.annotation.Timed;
+import org.metadatacenter.submission.biosample.AIRRTemplate;
 import org.metadatacenter.submission.biosample.core.BioSampleValidator;
 
 import javax.ws.rs.Consumes;
@@ -9,6 +10,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBException;
+import javax.xml.datatype.DatatypeConfigurationException;
+import java.io.IOException;
 
 @Path("/validate-airr") @Produces(MediaType.APPLICATION_JSON) @Consumes(MediaType.APPLICATION_JSON) public class AIRRBioSampleServerResource
 {
@@ -19,8 +23,16 @@ import javax.ws.rs.core.Response;
     this.bioSampleValidator = new BioSampleValidator();
   }
 
-  @POST @Timed public Response validate()
+  @POST @Timed public Response validate(AIRRTemplate airrInstance)
   {
-    return Response.ok().build(); // TODO
+    try {
+      return Response.ok(this.bioSampleValidator.validateAIRRInstance(airrInstance)).build();
+    } catch (JAXBException e) {
+      return Response.status(Response.Status.BAD_REQUEST).build();
+    } catch (IOException e) {
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    } catch (DatatypeConfigurationException e) {
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    }
   }
 }
