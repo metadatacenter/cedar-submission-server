257da1c1596274795b83ce1e3139df04e7d7a373
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-02-09 22:24:33 -0800

    Alpha release of new permission indexing feature

--- cedar-submission-server-application/src/main/java/org/metadatacenter/cedar/submission/SubmissionServerApplication.java
+++ cedar-submission-server-application/src/main/java/org/metadatacenter/cedar/submission/SubmissionServerApplication.java
@@ -1,19 +1,14 @@
 package org.metadatacenter.cedar.submission;
 
-import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import org.metadatacenter.bridge.CedarDataServices;
 import org.metadatacenter.cedar.submission.health.SubmissionServerHealthCheck;
 import org.metadatacenter.cedar.submission.resources.AIRRBioSampleServerResource;
 import org.metadatacenter.cedar.submission.resources.AMIA2016DemoBioSampleServerResource;
 import org.metadatacenter.cedar.submission.resources.IndexResource;
-import org.metadatacenter.cedar.util.dw.CedarDropwizardApplicationUtil;
-import org.metadatacenter.config.CedarConfig;
+import org.metadatacenter.cedar.util.dw.CedarMicroserviceApplication;
 
-public class SubmissionServerApplication extends Application<SubmissionServerConfiguration> {
-
-  protected static CedarConfig cedarConfig;
+public class SubmissionServerApplication extends CedarMicroserviceApplication<SubmissionServerConfiguration> {
 
   public static void main(String[] args) throws Exception {
     new SubmissionServerApplication().run(args);
@@ -21,25 +16,22 @@ public class SubmissionServerApplication extends Application<SubmissionServerCon
 
   @Override
   public String getName() {
-    return "submission-server";
+    return "cedar-submission-server";
   }
 
   @Override
-  public void initialize(Bootstrap<SubmissionServerConfiguration> bootstrap) {
-    cedarConfig = CedarConfig.getInstance();
-    CedarDataServices.getInstance(cedarConfig);
-
-    CedarDropwizardApplicationUtil.setupKeycloak();
+  public void initializeApp(Bootstrap<SubmissionServerConfiguration> bootstrap) {
   }
 
   @Override
-  public void run(SubmissionServerConfiguration configuration, Environment environment) {
+  public void runApp(SubmissionServerConfiguration configuration, Environment environment) {
 
     final IndexResource index = new IndexResource();
     environment.jersey().register(index);
 
     // Register resources
-    final AMIA2016DemoBioSampleServerResource amia2016DemoBioSampleServerResource = new AMIA2016DemoBioSampleServerResource();
+    final AMIA2016DemoBioSampleServerResource amia2016DemoBioSampleServerResource = new
+        AMIA2016DemoBioSampleServerResource();
     environment.jersey().register(amia2016DemoBioSampleServerResource);
 
     final AIRRBioSampleServerResource airrBioSampleServerResource = new AIRRBioSampleServerResource();
@@ -47,8 +39,5 @@ public class SubmissionServerApplication extends Application<SubmissionServerCon
 
     final SubmissionServerHealthCheck healthCheck = new SubmissionServerHealthCheck();
     environment.healthChecks().register("message", healthCheck);
-
-    CedarDropwizardApplicationUtil.setupEnvironment(environment);
-
   }
 }
