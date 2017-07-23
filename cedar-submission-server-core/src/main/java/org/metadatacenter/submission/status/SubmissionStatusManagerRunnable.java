653b36d39d122a4048178a86879761993eb70e69
martinjoconnor <martin.oconnor@stanford.edu>
2017-07-23 16:11:19 -0700

    More notification work

--- cedar-submission-server-core/src/main/java/org/metadatacenter/submission/status/SubmissionStatusManagerRunnable.java
+++ cedar-submission-server-core/src/main/java/org/metadatacenter/submission/status/SubmissionStatusManagerRunnable.java
@@ -68,7 +68,6 @@ public class SubmissionStatusManagerRunnable implements Runnable
         for (String submissionID : futures.keySet()) {
           Future<SubmissionStatus> future = futures.get(submissionID);
           future.cancel(true); // Cancel tasks that did not complete in time
-          submissionStatusManager.removeSubmission(submissionID);
           logger.warn("Status call for submission " + submissionID + " did not complete; cancelling");
         }
         futures.clear();
